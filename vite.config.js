5fac97e8b4968bc2ad57ac9ce5f1d43ed0256bb7
JoshuaOlubori <joshuaolubori@gmail.com>
2025-01-18 21:14:41 +0100

    fixed some build config issues

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: './src', // Ensure this points to the directory with index.html
   build: {
-    outDir: 'dist', // Output directory
+    outDir: '../docs', // Output directory
   },
 });
