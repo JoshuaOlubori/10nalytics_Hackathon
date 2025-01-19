140d411583219d30c8200cd05f2d8a2acb115fb9
JoshuaOlubori <joshuaolubori@gmail.com>
2025-01-19 19:38:45 +0100

    chore

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,24 +3,27 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     fontFamily: {
-      'bower': ["Bower"],
+      bower: ["Bower", "serif"],
     },
     extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+
       animation: {
-        'smoothBg': 'smoothBg 60s linear infinite',
+        smoothBg: "smoothBg 60s linear infinite",
       },
       keyframes: {
         smoothBg: {
-          'from': {
-            backgroundPosition: '50% 50%, 50% 50%'
+          from: {
+            backgroundPosition: "50% 50%, 50% 50%",
           },
-          'to': {
-            backgroundPosition: '350% 50%, 350% 50%'
-          }
-        }
-      }
-    }
-    
+          to: {
+            backgroundPosition: "350% 50%, 350% 50%",
+          },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
